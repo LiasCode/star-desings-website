0f6b8d98fd8c25b267cb65a85c6c3812a413c2ca
LiasCode <liasraul00@gmail.com>
2022-12-17 13:08:51 -0500

    add emails links in contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://star-designs.pages.dev",
-  integrations: [mdx(), sitemap()],
+  integrations: [sitemap()],
+  output: "static",
   server: {
     port: 9090,
     host: true,
+    open: true,
   },
 });
